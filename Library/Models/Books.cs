dee22e8abc77fbece173b19d5652128db373ab0e
Remya <remya.sudhakaran@gmail.com>
2020-09-02 14:37:47 -0400

    Add project files.

---
+++ Library/Models/Books.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }       
+        public DateTime Date_Published { get; set; }
+        public DateTime Date_Added { get; set; }
+
+        public int MyProperty { get; set; }
+        public String Category { get; set; }
+    }
+}
---
+++ Library/Models/Categories.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
